8b6c165904e22db77debcbf2e3b1a7e78d4f42dd
FelipeGuerrino <felipe_bg2005@hotmail.com>
2022-03-27 15:27:43 -0300

    started container structuring

---
+++ src/components/Container/index.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+class Container extends Component {
+    // state = {  } 
+    render() { 
+        return (
+            <>
+                {/* TODO: <h1>{categoryName}</h1> */}
+                {/* TODO: render conditionally the productCards */}
+            </>
+        );
+    }
+}
+ 
+export default Container;
